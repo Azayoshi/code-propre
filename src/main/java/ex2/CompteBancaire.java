dc368d83407e203c5f696f00b89e881dc6c4cb8a
LucasLournc <127390712+LucasLournc@users.noreply.github.com>
2023-03-29 17:36:32 +0200

    ex1 fait

--- src/main/java/ex2/CompteBancaire.java
+++ src/main/java/ex2/CompteBancaire.java
@@ -3,32 +3,14 @@ package ex2;
 /** Représente un compte bancaire de type compte courante (type=CC) ou livret A (type=LA)
  * @author DIGINAMIC
  */
-public class CompteBancaire {
+public class CompteBancaire{
 
-	private double solde;
-	private double decouvert;
-	private double tauxRemuneration;
-	private String type;
 
-	
-	/** Ce constructeur est utilisé pour créer un compte de type Livret A
-	 * @param type = LA
-	 * @param solde représente le solde du compte
-	 * @param decouvert  représente le découvert autorisé
-	 * @param tauxRemuneration  représente le taux de rémunération du livret A
-	 */
-	public CompteBancaire(String type, double solde, double decouvert, double tauxRemuneration) {
-		super();
-		this.type = type;
-		this.solde = solde;
-		this.decouvert = decouvert;
-		this.tauxRemuneration = tauxRemuneration;
-	}
-	
 	/** Ajoute un montant au solde
 	 * @param montant
 	 */
 	public void ajouterMontant(double montant){
+
 		this.solde += montant;
 	}
 	
@@ -59,60 +41,11 @@ public class CompteBancaire {
 		}
 	}
 
-
 	public void appliquerRemuAnnuelle(){
 		if (type.equals("LA")){
 			this.solde = solde + solde*tauxRemuneration/100;
 		}
 	}
 	
-	/** Getter for solde
-	 * @return the solde
-	 */
-	public double getSolde() {
-		return solde;
-	}
-	
-	/** Setter
-	 * @param solde the solde to set
-	 */
-	public void setSolde(double solde) {
-		this.solde = solde;
-	}
-	/** Getter for decouvert
-	 * @return the decouvert
-	 */
-	public double getDecouvert() {
-		return decouvert;
-	}
-	/** Setter
-	 * @param decouvert the decouvert to set
-	 */
-	public void setDecouvert(double decouvert) {
-		this.decouvert = decouvert;
-	}
-	/** Getter for tauxRemuneration
-	 * @return the tauxRemuneration
-	 */
-	public double getTauxRemuneration() {
-		return tauxRemuneration;
-	}
-	/** Setter
-	 * @param tauxRemuneration the tauxRemuneration to set
-	 */
-	public void setTauxRemuneration(double tauxRemuneration) {
-		this.tauxRemuneration = tauxRemuneration;
-	}
-	/** Getter for type
-	 * @return the type
-	 */
-	public String getType() {
-		return type;
-	}
-	/** Setter
-	 * @param type the type to set
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
+
 }
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,85 @@
 package ex2;
 
-public class LivretA extends CompteBancaire {
+public abstract class LivretA {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	private String type ;
+	private double solde;
+	private double tauxRemuneration ;
+	private int decouvert = 0 ;
+
+	/**
+	 * constructeur de livretA
+	 * @param type
+	 * @param solde
+	 * @param tauxRemuneration
+	 * @param decouvert
+	 */
+	public LivretA(String type, double solde, double tauxRemuneration, int decouvert) {
+		this.type = type;
+		this.solde = solde;
+		this.tauxRemuneration = tauxRemuneration;
+		this.decouvert = decouvert;
+	}
+
+
+	/**
+	 * get
+ 	 * @return type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * set
+	 * @return type
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * get
+	 * @return solde
+	 */
+	public double getSolde() {
+		return solde;
+	}
+	/**
+	 * set
+	 * @return solde
+	 */
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	/**
+	 * get
+	 * @return tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	/**
+	 * set
+	 * @return tauxRemuneration
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
+
+	/**
+	 * get
+	 * @return decouvert
+	 */
+	public int getDecouvert() {
+		return decouvert;
+	}
+	/**
+	 * set
+	 * @return decouvert
+	 */
+	public void setDecouvert(int decouvert) {
+		this.decouvert = decouvert;
 	}
 
 }
